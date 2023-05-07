133b689a4dd1d280905bb2fcb95402404db6d40e
gonazmul <gonazmul@gmail.com>
2023-05-07 15:01:10 -0400

    learned about waits and practices more schenarios

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# selenium
+# Selenium
+
+### Different Types of waits in selenium
+
+There are 4 types of waits in selenium -
+
+1. Sleep: This ia very hard wait. Example, `Thread.sleep(5000)`
+2. Implicit wait: It is to tell selenium to wait for certain time before it throws exception. 
+   Implicit wait is valid for the life of the webdriver.
+3. Explicit wait: Explicit wait is to tell the selenium/driver to wait certain time
+   until certain condition occurs before throwing the exception.
+4. Fluent wait: Fluent wait is to tell the selenium/driver to wait maximum amount of time for certain condition.
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,20 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("User should be successfully logged in and logged out.");
 
+        // Initiate Selenium WebDriver
         WebDriver driver;
 
         // Setup chrome browser
@@ -17,6 +24,9 @@ public class Main {
 
         // Open a chrome browser
         driver = new ChromeDriver();
+        
+        // Implicit Wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
 
         // Navigate to the url https://www.saucedemo.com/
         driver.get("https://www.saucedemo.com/");
@@ -39,9 +49,20 @@ public class Main {
         }
 
         // Logout
-        Thread.sleep(2000);
+        // Explicit wait
+        // Wait for hamburger menu to be clickable untill 5000 milliseconds.
+        // It will keep checking every 500 milliseconds to the clickability until the time out of 5000 milliseconds.
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("#react-burger-menu-btn"))));
+
         driver.findElement(By.cssSelector("#react-burger-menu-btn")).click();
-        Thread.sleep(2000);
+
+        // Fluent Wait
+        FluentWait wait1 = new FluentWait(driver);
+        wait1.withTimeout(Duration.ofMillis(10000));
+        wait1.pollingEvery(Duration.ofMillis(2000));
+        wait1.ignoring(ElementNotInteractableException.class);
+
         driver.findElement(By.cssSelector("#logout_sidebar_link")).click();
 
         // Wait 5 seconds
